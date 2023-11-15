39bc72f355cc2b53b1f10d8fb64f6bcb6068f25c
bailebao3 <elizazhai@gmail.com>
2023-11-15 15:54:10 -0800

    fix bugs in booksRoute

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bookstore_MERN/", // Replace <REPO> with your repository name
   plugins: [react()],
 });
